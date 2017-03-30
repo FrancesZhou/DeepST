40af131e5cf0fb3a5ff46cc9e98a2556f8a93d5d
Frances Zhou <zhouxian@sjtu.edu.cn>
2017-03-30 05:40:07 -0400

    iLayer.py fix

--- deepst/models/iLayer.py
+++ deepst/models/iLayer.py
@@ -11,13 +11,14 @@ class iLayer(Layer):
 
     def build(self, input_shape):
         #initial_weight_value = np.random.random(input_shape[1:])
-        initial_weight_value = np.random.random(input_shape[:2])
+	print(input_shape)
+        initial_weight_value = np.random.random(input_shape[1:3])
         self.W = K.variable(initial_weight_value)
         self.trainable_weights = [self.W]
 
     def call(self, x, mask=None):
         #return x * self.W
-        return x * K.expand_dims(self.W, axis=-1)
+        return x*K.tile(K.expand_dims(self.W, axis=-1),[1,1,x.get_shape().as_list()[-1]])
 
     def get_output_shape_for(self, input_shape):
         return input_shape
--- scripts/papers/AAAI17/BikeNYC/exptBikeNYC.py
+++ scripts/papers/AAAI17/BikeNYC/exptBikeNYC.py
@@ -77,16 +77,17 @@ def build_model(external_dim):
 def main():
     # load data
     print("loading data...")
-    #X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = BikeNYC.load_data(
-    #    T=T, nb_flow=nb_flow, len_closeness=len_closeness, len_period=len_period, len_trend=len_trend, len_test=len_test,
-    #    preprocess_name='preprocessing.pkl', meta_data=True)
+    X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = BikeNYC.load_data(
+        T=T, nb_flow=nb_flow, len_closeness=len_closeness, len_period=len_period, len_trend=len_trend, len_test=len_test,
+        preprocess_name='preprocessing.pkl', meta_data=True)
 
-    #print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
+    print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
 
     print('=' * 10)
     print("compiling model...")
     print(
         "**at the first time, it takes a few minites to compile if you use [Theano] as the backend**")
+    #external_dim = 8
     model = build_model(external_dim)
     hyperparams_name = 'c{}.p{}.t{}.resunit{}.lr{}'.format(
         len_closeness, len_period, len_trend, nb_residual_unit, lr)
